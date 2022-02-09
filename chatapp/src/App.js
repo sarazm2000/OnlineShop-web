8cd255f7addc70a4a02f1b14bfbc044fb7806585
Dariush <59167222+dariusamiri@users.noreply.github.com>
2022-02-09 17:56:34 +0330

    Update App.js

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -4,7 +4,7 @@ import ChatFeed from './components/ChatFeed.jsx';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-const projectID = '4f227b37-af25-4215-ad39-4c93b7a168bb';
+const projectID = '262c0d23-121f-4302-8d43-6de1d5cd997a';
 
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
